33691fb99f22581c29e0622f862780a288912478
harsh-si14 <harsh.vardhan1_cs20@gla.ac.in>
2022-12-25 13:34:14 +0530

    tyu

---
+++ index.js
@@ -0,0 +1,29 @@
+const img = document.querySelector('.main-img img');
+const toggleBtn = document.querySelector('.toggle');
+const toggleText = document.querySelectorAll('.toggle-text');
+
+let bool = true;
+
+toggleBtn.addEventListener('click', () =>{
+
+    if(bool === true){
+        img.src = "./cup-light.png";
+
+        toggleText.forEach(text =>{
+            text.innerText="White";
+        });
+        bool = false;
+    }
+    else{
+        img.src = "./cup-dark.png";
+
+        toggleText.forEach(text =>{
+            text.innerText="Black";
+        });
+        bool = true;
+    }
+
+    document.body.classList.toggle('light-theme');
+
+    toggleBtn.firstElementChild.classList.toggle('fa-sun');
+})
